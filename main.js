af4d564a4d4897fe091acf5f8357a2e571c9e47e
BuildTools <maciej.bodnar2@gmail.com>
2024-03-29 18:12:08 +0100

    adding modal 2, loading svg

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ console.log("CoockieConsent Project");
 
 const modalEl = document.getElementById("modal");
 const modalCloseBtn = document.getElementById("modal-close-btn");
+const consentForm = document.getElementById("consent-form");
+const modalText = document.getElementById("modal-text");
 
 // Modal Opener
 setTimeout(function () {
@@ -11,4 +13,22 @@ setTimeout(function () {
 
 modalCloseBtn.addEventListener("click", function () {
   modalEl.style.display = "none";
+});
+
+consentForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("prevented");
+
+  modalText.innerHTML = `
+                      <div class="modal-inner-loading">
+                          <img src="img/loading.svg" class="loading">
+                          <p id="uploadText">
+                              Uploading your data to the dark web...
+                          </p>
+                      </div>
+  `;
+
+  setTimeout(function () {
+    document.getElementById("uploadText").textContent = "Making the sale...";
+  }, 1500);
 });
