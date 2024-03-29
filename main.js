b6f222f0fcad8958fc4e12c74984e2fe5e6606c5
BuildTools <maciej.bodnar2@gmail.com>
2024-03-29 14:45:59 +0100

    opening popup after 1,5s

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("CoockieConsent Project");
+
+const modalEl = document.getElementById("modal");
+
+// Modal Opener
+setTimeout(function () {
+  console.log("modalDisplayBlock");
+  modalEl.style.display = "block";
+}, 1500);
