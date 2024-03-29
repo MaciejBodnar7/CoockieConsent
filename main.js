bfec0980820b87cb91d25ca71e346731cb870e50
BuildTools <maciej.bodnar2@gmail.com>
2024-03-29 16:15:43 +0100

    adding close x popup

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 console.log("CoockieConsent Project");
 
 const modalEl = document.getElementById("modal");
+const modalCloseBtn = document.getElementById("modal-close-btn");
 
 // Modal Opener
 setTimeout(function () {
   console.log("modalDisplayBlock");
   modalEl.style.display = "block";
 }, 1500);
+
+modalCloseBtn.addEventListener("click", function () {
+  modalEl.style.display = "none";
+});
