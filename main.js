33bf18be4289e1e6e43fa49235cc8acb56fcb0fa
MaciejBodnar <maciej.bodnar2@gmail.com>
2024-03-30 15:49:33 +0100

    adding new modals in js, styling, storing data from form

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const modalEl = document.getElementById("modal");
 const modalCloseBtn = document.getElementById("modal-close-btn");
 const consentForm = document.getElementById("consent-form");
 const modalText = document.getElementById("modal-text");
+const modalMain = document.getElementById("modal-inner");
 
 // Modal Opener
 setTimeout(function () {
@@ -17,7 +18,9 @@ modalCloseBtn.addEventListener("click", function () {
 
 consentForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log("prevented");
+
+  const consentFormData = new FormData(consentForm); //storing data from form in this variable
+  console.log(consentFormData);
 
   modalText.innerHTML = `
                       <div class="modal-inner-loading">
@@ -31,4 +34,15 @@ consentForm.addEventListener("submit", function (e) {
   setTimeout(function () {
     document.getElementById("uploadText").textContent = "Making the sale...";
   }, 1500);
+
+  setTimeout(function () {
+    modalMain.innerHTML = `
+                      <h2>Thanks you sucker! </h2>
+                      <p>We just sold the rights to your eternal soul.</p>
+                      <div class="idiot-gif">
+                          <img src="img/pirate.gif">
+                      </div>
+`;
+    consentForm.style.display = "none";
+  }, 3000);
 });
